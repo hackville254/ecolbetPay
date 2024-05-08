9f301399c624b4bf3967909539491dde2f5e03f9
hackville254 <fdelbo47@gmail.com>
2024-05-08 09:27:05 +0200

    deploiement sans base de donne

--- Dockerfile
+++ Dockerfile
@@ -24,4 +24,4 @@ COPY . /ecolebetPay/
 EXPOSE 8923
 
 # Commande pour démarrer l'application Django
-# CMD ["gunicorn", "--bind", "0.0.0.0:8913", "chantier237_Api.wsgi:application"]
+#CMD ["gunicorn", "--bind", "0.0.0.0:8923", "ecolbetPay.wsgi:application"]
--- ecolbetPay/settings.py
+++ ecolbetPay/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
 ]
 CORS_ALLOWED_ORIGINS = [
     "https://*",
+    "http://*",
 ]
 
 ROOT_URLCONF = 'ecolbetPay.urls'
@@ -88,24 +89,24 @@ WSGI_APPLICATION = 'ecolbetPay.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': config('DATABASE_ENGINE'),
+#         'NAME': config('DATABASE_NAME'),
+#         'USER': config('DATABASE_USER'),
+#         'PASSWORD': config('DATABASE_PASSWORD'),
+#         'HOST': config('DATABASE_HOST'),
+#         'PORT': config('DATABASE_PORT', cast=int),
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': config('DATABASE_ENGINE'),
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT', cast=int),
-    }
-}
-
-
-""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} """
+}
 
 
 # Password validation
